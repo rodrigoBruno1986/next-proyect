1b52312c003797167b4356a881b64b721a3779f6
rodrigo.sebastian <rodrigo.sebastian@globalsys.com.br>
2024-07-18 13:31:09 -0300

    change nav link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,6 +8,7 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import React from 'react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
